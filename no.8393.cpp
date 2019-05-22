26b7a32e8dc6bc312a7b4e56d82957e87bb41270
SophieYoonseo <45335586+SophieYoonseo@users.noreply.github.com>
2019-05-22 21:45:30 +0900

    합구하기

---
+++ no.8393.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin>>n;
+	int sum=0;
+	
+	for(int i=1; i<=n; i++)
+		sum += i;
+	cout<<sum<<"\n";	
+}
