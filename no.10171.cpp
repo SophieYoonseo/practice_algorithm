07e852e3a9d5370b9258990746644c4e9cbf328f
SophieYoonseo <45335586+SophieYoonseo@users.noreply.github.com>
2019-07-05 13:34:51 +0900

    고양이
    #단계별로풀어보기
    #입출력과사칙연산
    
    \  '을 따로 구분해줘야함.

---
+++ no.10171.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cout<<"\\    /\\"<<endl;
+	cout<<" )  ( \')"<<endl;
+	cout<<"(  /  )"<<endl;
+	cout<<" \\(__)|"<<endl;
+	return 0;
+}
